98d6981a565b0ed33dceb776c97a14a0faad87a7
brianpopquiz <brian@popquizcomputers.com>
2025-05-02 10:59:06 -0400

    Update App.jsx
    Added vendor call to tracker

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const CLIENTS = [
   "Novick", "Fabio", "Sullivans", "Pro Storm", "Metal and Wood", "DDS",
   "Foglia", "Northeast Fence", "Steel Penny", "Super Impulse", "Pennypack",
   "St Doms", "Ferreira Law", "Pollack Law", "Email Support", "Break Fix",
-  "Residential Member", "Residential", "M&T", "Make and Take", "Your Approved Contractors", "Winderco", "Groom'n Room"
+  "Residential Member", "Residential", "M&T", "Make and Take", "Your Approved Contractors", "Winderco", "Groom'n Room", "Vendor Call"
 ];
 
 function formatESTTime(isoTime) {
