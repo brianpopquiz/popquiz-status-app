6c8a93b123e44732f6ff50931121982cb5b5b049
brianpopquiz <brian@popquizcomputers.com>
2025-06-18 11:08:56 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import Reports from './Reports.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+...
+
+<Reports />
